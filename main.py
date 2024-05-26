18e6dd1bc3bc554fff178f806956650f1c68ae2a
erclem1998 <61994737+erclem1998@users.noreply.github.com>
2024-05-25 21:03:57 -0600

    Se ha agregado algunos mensajes en ingles (#2)
    Co-authored-by: Erick Lemus <erick.lemus1998@gmail.com>

--- main.py
+++ main.py
@@ -28,8 +28,8 @@ if __name__ == "__main__":
         filename = sys.argv[1]
         remove_duplicates = sys.argv[2].lower() == "yes"
     else:
-        print("Se debe indicar el fichero como primer argumento")
-        print("El segundo argumento indica si se quieren eliminar duplicados")
+        print("You must indicate the file as first argument")
+        print("Second argument says if you want to delete duplicates")
         sys.exit(1)
 
     print(f"Se leerán las palabras del fichero {filename}")
